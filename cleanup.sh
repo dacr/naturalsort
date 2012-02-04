83f4e1986bbfad902daddbc8e1cb9378452c6092
Crosson David <crosson.david@gmail.com>
2012-02-04 22:39:04 +0100

    small change

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 find . -name "*~" -exec rm {} \;
-rm -fr target project/target project/boot project/project nohup.out
+rm -fr target project/target project/boot project/project
+rm -f .classpath .project .cache
 

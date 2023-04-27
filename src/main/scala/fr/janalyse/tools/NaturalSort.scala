340d4196c82ed6edf8e573bc38479526c6f1bb25
Crosson David <crosson.david@gmail.com>
2023-04-27 08:58:50 +0200

    Add scalafmt

--- src/main/scala/fr/janalyse/tools/NaturalSort.scala
+++ src/main/scala/fr/janalyse/tools/NaturalSort.scala
@@ -18,19 +18,25 @@ package fr.janalyse.tools
 import scala.annotation.tailrec
 
 object NaturalSort {
-  
+
   implicit val ord: Ordering[String] = new Ordering[String] {
     //def groupIt(str:String) = if (str.nonEmpty && str.head.isDigit) str.takeWhile(_.isDigit) else str.takeWhile(!_.isDigit)
-    def groupIt(str:String):String = if (str.nonEmpty && str.head.isDigit) str.takeWhile(_.isDigit) else str.substring(0,if (str.nonEmpty) 1 else 0)
-    private val dec="""(\d+)""".r
+    def groupIt(str: String): String = if (str.nonEmpty && str.head.isDigit) str.takeWhile(_.isDigit) else str.substring(0, if (str.nonEmpty) 1 else 0)
+
+    private val dec = """(\d+)""".r
+
     @tailrec
-    def compare(str1: String, str2: String):Int = {
+    def compare(str1: String, str2: String): Int = {
       (groupIt(str1), groupIt(str2)) match {
-        case ("","") => 0
-        case (dec(x),dec(y)) if x.toLong==y.toLong =>  compare(str1.substring(x.length), str2.substring(y.length))
-        case (dec(x),dec(y)) => x.toLong - y.toLong match { case 0L => 0 case x if x<0 => -1 case _ => 1}
-        case (x,y) if x == y => compare(str1.substring(x.length), str2.substring(y.length))
-        case (x,y) => x compareTo y
+        case ("", "") => 0
+        case (dec(x), dec(y)) if x.toLong == y.toLong => compare(str1.substring(x.length), str2.substring(y.length))
+        case (dec(x), dec(y)) => x.toLong - y.toLong match {
+          case 0L => 0
+          case x if x < 0 => -1
+          case _ => 1
+        }
+        case (x, y) if x == y => compare(str1.substring(x.length), str2.substring(y.length))
+        case (x, y) => x compareTo y
       }
     }
   }
